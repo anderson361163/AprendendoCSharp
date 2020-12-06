57e5e524489f45d6c50feb7c560bd214408d7573
anderson361163 <anderson361163@gmail.com>
2020-12-06 16:49:34 -0300

    Resolvendo o exercicio

--- 8-CondicionaisParte2/Program.cs
+++ 8-CondicionaisParte2/Program.cs
@@ -13,12 +13,16 @@ namespace _8_CondicionaisParte2
             double salario = 3300.0;
 
             if (salario < 2600.0)
+            {
                 Console.WriteLine("A sua aliquota é de 15%");
-            Console.WriteLine("Você pode deduzir até R$ 350");
+                Console.WriteLine("Você pode deduzir até R$ 350");
+            }
 
             if (salario < 3750.0)
+            {
                 Console.WriteLine("A sua aliquota é de 22,5%");
-            Console.WriteLine("Você pode deduzir até R$ 636");
+                Console.WriteLine("Você pode deduzir até R$ 636");
+            }
 
             Console.WriteLine("Pressione a tecla enter para finalizar o programa . . .");
             Console.ReadLine();
