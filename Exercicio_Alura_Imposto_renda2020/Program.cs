daa79ec90208c3c329b0236a4ba73e1dd4b15514
anderson361163 <anderson361163@gmail.com>
2020-12-06 17:15:30 -0300

    Exercício da Alura para ajudar o João a fazer um programa para calcular a aliquota de o IR dele com uma variavel com valor previamente definida.

---
+++ Exercicio_Alura_Imposto_renda2020/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Alura_Imposto_renda2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 2261.00;
+
+            if (salario <= 1903.98)
+            {
+                Console.WriteLine("Você é isento do imposto de renda!");
+            }
+            else if(salario > 1903.99 || salario <= 2826.65)
+            {
+                Console.WriteLine("A sua aliquota é de 7,5%");
+                Console.WriteLine("Você pode deduzir até R$ 142,80");
+            }
+            else if (salario > 2826.66 || salario <= 3751.05)
+            {
+                Console.WriteLine("A sua aliquota é de 15%");
+                Console.WriteLine("Você pode deduzir até R$ 354,80");
+            }
+            else if (salario > 3751.06 || salario <= 4664.68)
+            {
+                Console.WriteLine("A sua aliquota é de 22,5%");
+                Console.WriteLine("Você pode deduzir até R$ 636,13");
+            }
+            else if (salario > 4664.68)
+            {
+                Console.WriteLine("A sua aliquota é de 27,5%");
+                Console.WriteLine("Você pode deduzir até R$ 869,36");
+            }
+            else
+            {
+                Console.WriteLine("ERRO");
+            }
+
+            Console.WriteLine(@"Referencia: https://www.gov.br/receitafederal/pt-br/assuntos/orientacao-tributaria/tributos/irpf-imposto-de-renda-pessoa-fisica");
+
+            Console.WriteLine("Pressione a tecla enter para finalizar o programa . . .");
+            Console.ReadLine();
+        }
+    }
+}
