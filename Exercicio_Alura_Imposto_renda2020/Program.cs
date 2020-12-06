4837e885351d5815f83dafa1006328175941cfa9
anderson361163 <anderson361163@gmail.com>
2020-12-06 17:17:54 -0300

    Efetuando a correção do exercício

--- Exercicio_Alura_Imposto_renda2020/Program.cs
+++ Exercicio_Alura_Imposto_renda2020/Program.cs
@@ -16,17 +16,17 @@ namespace Exercicio_Alura_Imposto_renda2020
             {
                 Console.WriteLine("Você é isento do imposto de renda!");
             }
-            else if(salario > 1903.99 || salario <= 2826.65)
+            else if(salario > 1903.99 && salario <= 2826.65)
             {
                 Console.WriteLine("A sua aliquota é de 7,5%");
                 Console.WriteLine("Você pode deduzir até R$ 142,80");
             }
-            else if (salario > 2826.66 || salario <= 3751.05)
+            else if (salario > 2826.66 && salario <= 3751.05)
             {
                 Console.WriteLine("A sua aliquota é de 15%");
                 Console.WriteLine("Você pode deduzir até R$ 354,80");
             }
-            else if (salario > 3751.06 || salario <= 4664.68)
+            else if (salario > 3751.06 && salario <= 4664.68)
             {
                 Console.WriteLine("A sua aliquota é de 22,5%");
                 Console.WriteLine("Você pode deduzir até R$ 636,13");
