1300bcc607655cc970a00e520b66292c16655a14
anderson361163 <anderson361163@gmail.com>
2020-12-06 16:53:52 -0300

    Novo exercicio da Alura e resolução dele

---
+++ 9-CondicionaisParte3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_CondicionaisParte3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool foiPromovido = true;
+            double salario = 0.0;
+
+
+            if (foiPromovido)
+            {
+                salario = 4200.0;
+            }
+            else
+            {
+                salario = 3800.0;
+            }
+
+            Console.WriteLine(salario);
+
+            Console.WriteLine("Pressione a tecla enter para finalizar o programa . . .");
+            Console.ReadLine();
+        }
+    }
+}
