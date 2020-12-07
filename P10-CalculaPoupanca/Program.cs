16a7ccd71c26ee0767fcee421e0618795c2b039c
anderson361163 <anderson361163@gmail.com>
2020-12-06 21:51:17 -0300

    Exercicios

---
+++ P10-CalculaPoupanca/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 10 - Calcula poupança");
+
+            double valorInvestido = 1000;
+            for (int i =1; i <= 12; i++)
+            {   
+                valorInvestido *= 1.0036;
+                Console.WriteLine("Após {0} meses, você terá {1} aplicados.", i, valorInvestido.ToString("C3"));
+            }
+
+            Console.WriteLine("Pressione a tecla enter para finalizar o programa . . . ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P12-CalculaInvestimentoALongoPrazo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P12_CalculaInvestimentoALongoPrazo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 11 - Calcula poupança a longo prazo");
+
+            double valorInvestido = 1000;
+            double fatorInvestimento = 1.0036;
+
+            for (int contadorAno = 1; contadorAno <= 5; contadorAno++)
+            {
+                for (int contadorMes = 1; contadorMes <=12; contadorMes++)
+                {
+                    valorInvestido *= fatorInvestimento;
+                }
+                fatorInvestimento += 0.0010;
+            }
+
+            Console.WriteLine("Ao termino do investimento você terá {0}!", valorInvestido.ToString("C2"));
+            Console.WriteLine("Pressione a tecla enter para finalizar o programa . . . ");
+            Console.ReadLine();
+        }
+    }
+}
